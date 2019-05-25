bf902fdffb80d7da1f5a41d7743013ee4e83d8a6
Gustavo Miranda <gustavo.miranda@acad.pucrs.br>
2019-05-25 18:28:17 -0300

    setup

---
+++ src/main/java/br/com/mirandalabs/urlchecker/UrlCheckerApplication.java
@@ -0,0 +1,13 @@
+package br.com.mirandalabs.urlchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlCheckerApplication.class, args);
+	}
+
+}
